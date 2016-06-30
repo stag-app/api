61b5fa31ae252490d55cb1c5ff380eb08eb3400e
Bart Langelaan <langelaanbart@gmail.com>
2016-06-30 13:31:54 +0200

    Add our ID's

--- routes/api.js
+++ routes/api.js
@@ -10,8 +10,16 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/nfc', (req, res) => {
+  var id = req.query.id;
+
+  if(id == "-27_37_25_76") id = "Bart Langelaan";
+  if(id == "21_-100_87_107") id = "Samson Evertsz";
+  if(id == "87_125_-41_58") id = "Lisa Uijtewaal";
+  if(id == "17_-61_-41_56") id = "Plaisir Nzuzi";
+  if(id == "117_-115_36_-36") id = "Tom Maarleveld";
+
   data.push({
-    id: req.query.id,
+    id: id,
     location: "Station Rotterdam",
     count: 0,
     date: new Date()
